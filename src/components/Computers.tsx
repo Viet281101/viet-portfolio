419b0193d4257e39bf0137a32781efb52dee6ac0
Viet281101 <nva281101@gmail.com>
2024-07-24 02:00:07 +0200

    update computer model view

--- src/components/Computers.tsx
+++ src/components/Computers.tsx
@@ -3,16 +3,55 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "./Loader";
-import desktop_pc from "/desktop_pc/scene.gltf";
+import desktop_pc from "/desktop_pc/scene.glb?url";
 
-const Computers = () => {
-	const computer = useGLTF(desktop_pc)
+interface ComputersProps {
+	isMobile: boolean;
+};
+
+const Computers = ({ isMobile }: ComputersProps) => {
+	const computer = useGLTF(desktop_pc);
 
 	return (
 		<mesh>
-			<hemisphereLight intensity={0.15} groundColor={"black"} />
+		<hemisphereLight intensity={0.15} groundColor={"black"} />
+		<pointLight intensity={20} />
+		<spotLight position={[0, 50, 0]} angle={0.3} penumbra={1} intensity={2} castShadow shadow-mapSize={1024} />
+		<primitive object={computer.scene} scale={isMobile ? 1.2 : 1} position={isMobile ? [0, -4, -1] : [2, -1, -2]} rotation={[0, 0, 0]} />
 		</mesh>
 	);
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+		// Set initial value
+		setIsMobile(mediaQuery.matches);
+
+		// Define callback function to handle media query changes
+		const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+			setIsMobile(event.matches);
+		};
+
+		// Add event listener for media query changes
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+		// Cleanup event listener on component unmount
+		return () => { mediaQuery.removeEventListener("change", handleMediaQueryChange); };
+	}, []);
+
+	return (
+		<Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+		<Suspense fallback={<CanvasLoader />}>
+			<OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+			<Computers isMobile={isMobile} />
+		</Suspense>
+		<Preload all />
+		</Canvas>
+	);
+};
+
+export default ComputersCanvas;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,3 @@
 /// <reference types="vite/client" />
-declare module "*.gltf" {
-	const value: string;
-	export default value;
-}
+declare module "*.glb" { const value: string; export default value; };
+declare module "*.gltf" { const value: string; export default value; };
