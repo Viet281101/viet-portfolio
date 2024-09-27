93d2b29e6d1349bc000cf978a05fb6010271b6bc
Viet281101 <nva281101@gmail.com>
2024-09-27 19:26:54 +0200

    remake about styles

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import IntroSection from '../components/IntroSection';
-import SkillsSection from '../components/SkillsSection';
-import ExperienceSection from '../components/ExperienceSection';
+import React, { useEffect, useRef, useState } from 'react';
 
 const About: React.FC = () => {
+	const titleRef = useRef<HTMLDivElement>(null);
+	const subtitleRef = useRef<HTMLDivElement>(null);
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setIsVisible(true);
+				} else {
+					setIsVisible(false);
+				}
+			});
+		},{ threshold: 0.5 });
+
+		if (titleRef.current) observer.observe(titleRef.current);
+		if (subtitleRef.current) observer.observe(subtitleRef.current);
+
+		return () => {
+			if (titleRef.current) observer.unobserve(titleRef.current);
+			if (subtitleRef.current) observer.unobserve(subtitleRef.current);
+		};
+	}, []);
+
 	return (
 		<div className="about-container">
-			{/* Section 1: Intro & Model 3D avatar */}
-			<IntroSection />
-
-			{/* Section 2: Skills */}
-			<SkillsSection />
-
-			{/* Section 3: Experiences (Timeline Experience) */}
-			<ExperienceSection />
+		<div ref={titleRef} className={`about-title ${isVisible ? 'animate-zoom-appear' : ''}`}>
+			Quick look about me
+		</div>
+		<div ref={subtitleRef} className={`about-subtitle ${isVisible ? 'animate-slide-right' : ''}`}>
+			Scroll Down
+		</div>
+		{/* Orther */}
 		</div>
 	);
 };
